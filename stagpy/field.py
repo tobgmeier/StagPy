6c92083797d502e2e2551828206be29a4c09d53b
Tobias Meier <tobias.meier@csh.unibe.ch>
2020-11-10 09:56:46 +0100

    star label position change

--- stagpy/field.py
+++ stagpy/field.py
@@ -315,7 +315,7 @@ def plot_scalar(step, var, field=None, axis=None,print_time = None, print_subste
         cax2.text(0.48, 0.4, 'Day', horizontalalignment='right', verticalalignment='center',color=text_color, size = text_size,transform=cax2.transAxes)
         cax2.text(0.52, 0.4, 'Night', horizontalalignment='left', verticalalignment='center',color=text_color, size = text_size, transform=cax2.transAxes)
         bbox_props = dict(boxstyle="rarrow", ec="black", lw=0.5,fc='y')
-        axis.text(-radius-0.095*radius, 0.0, "STAR", ha="right", va="center",bbox=bbox_props,size = text_size, color=text_color)
+        axis.text(-radius-0.068*radius, 0.0, "STAR", ha="right", va="center",bbox=bbox_props,size = text_size, color=text_color)
         axis.text(rda , 0.5, "0$\degree$", ha="left", va="center", color=text_color,size=text_size,transform=axis.transAxes)
         axis.text(1-rda+0.005, 0.5, "180$\degree$", ha="right", va="center", color=text_color,size=text_size,transform=axis.transAxes)
         axis.text(0.5 , 1-rda, "90$\degree$", ha="center", va="top", color=text_color,size=text_size,transform=axis.transAxes)
